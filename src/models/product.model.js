05ed2438aa8d02ab9bae409906ff4ee2fd59dcf3
ngotienthien <ngotienthienntt@gmail.com>
2023-10-11 14:59:54 +0700

    section12/product-schema

---
+++ src/models/product.model.js
@@ -0,0 +1,94 @@
+"use strict"
+
+//!dmbg
+const { Schema, model, Types } = require("mongoose"); // Erase if already required
+
+const PRODUCT_DOCUMENT_NAME = "Product"
+const PRODUCT_COLLECTION_NAME = "Products"
+
+const CLOTHING_DOCUMENT_NAME = "Clothing"
+const CLOTHING_COLLECTION_NAME = "Clothings"
+
+const ELECTRONIC_DOCUMENT_NAME = "Electronic"
+const ELECTRONIC_COLLECTION_NAME = "Electronics"
+
+// Declare the Schema of the Mongo model
+const ProductSchema = new Schema({
+    product_name: {
+        type: String,
+        required: true,
+    },
+
+    product_thumb: {
+        type: String,
+        required: true,
+    },
+
+    product_description: {
+        type: String,
+    },
+
+    product_price: {
+        type: Number,
+        required: true
+    },
+
+    product_quantity: {
+        type: Number,
+        required: true
+    },
+
+    product_type: {
+        type: String,
+        required: true,
+        enum: ["Electronics","Clothing", "Furniture"]
+    },
+
+    product_shop: {
+        type: Schema.Types.ObjectId,
+        ref: "shop"
+    },
+
+    product_attributes: {
+        type: Schema.Types.Mixed,
+        required: true
+    },
+}, {
+    timestamps: true,
+    collection: PRODUCT_COLLECTION_NAME
+});
+
+//define product type = clothing
+
+const clothingSchema = new Schema({
+    brand: {
+        type: String,
+        require: true,
+    },
+    size: String,
+    material: String,
+}, {
+    timestamps: true,
+    collection: CLOTHING_COLLECTION_NAME
+})
+
+//define product type = electronic
+
+const electronicSchema = new Schema({
+    manufacturer: {
+        type: String,
+        require: true,
+    },
+    model: String,
+    color: String,
+}, {
+    timestamps: true,
+    collection: ELECTRONIC_COLLECTION_NAME
+})
+
+//Export the model
+module.exports ={
+    product: model(PRODUCT_DOCUMENT_NAME, ProductSchema),
+    electronic: model(ELECTRONIC_DOCUMENT_NAME, electronicSchema),
+    clothing: model(CLOTHING_DOCUMENT_NAME, clothingSchema)
+} 
